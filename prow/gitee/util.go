1d4546b829e56c14b28dd710004dee8ab1d9f5e7
xwzQmxx <1499273991@qq.com>
2021-04-21 09:45:44 +0800

    fix function param error

--- prow/gitee/util.go
+++ prow/gitee/util.go
@@ -10,6 +10,6 @@ func GetOwnerAndRepoByPREvent(pre *sdk.PullRequestEvent) (string, string) {
 }
 
 //GetOwnerAndRepoByIssueEvent obtain the owner and repository name from the issue's event
-func GetOwnerAndRepoByIssueEvent(pre *sdk.PullRequestEvent) (string, string) {
-	return pre.Repository.Namespace, pre.Repository.Path
+func GetOwnerAndRepoByIssueEvent(issue *sdk.IssueEvent) (string, string) {
+	return issue.Repository.Namespace, issue.Repository.Path
 }
